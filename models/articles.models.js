a2a01a9acb0e9d0ece1428fc083888ac3071e867
Will <billyjoe2701@gmail.com>
2023-11-20 14:23:51 +0000

    Gets all articles and adds a comment count variable

--- models/articles.models.js
+++ models/articles.models.js
@@ -15,28 +15,17 @@ exports.getArticleById = (id) => {
 };
 
 exports.getArticles = () => {
-  return db.query(
-    `SELECT 
-    articles.author,
-    articles.title,
-    articles.article_id,
-    articles.topic,
-    articles.created_at,
-    articles.votes,
-    articles.article_img_url,
-    CAST(COUNT(comments.comment_id)AS INT) AS comment_count
+  return db
+    .query(
+      `SELECT articles.author,articles.title,articles.article_id,articles.topic,
+    articles.created_at,articles.votes,articles.article_img_url,CAST(COUNT(comments.comment_id)AS INT) AS comment_count
 FROM articles
 LEFT JOIN comments ON comments.article_id = articles.article_id
 GROUP BY 
-    articles.author,
-    articles.title,
-    articles.article_id,
-    articles.topic,
-    articles.created_at,
-    articles.votes,
-    articles.article_img_url
+    articles.article_id
 ORDER BY articles.created_at DESC;`
-  ).then(({rows})=> {
-    return rows
-  });
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
 };
