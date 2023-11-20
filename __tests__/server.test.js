901984ffbdfc0ce765079f4724b176a914a9ce2d
Will <billyjoe2701@gmail.com>
2023-11-20 10:52:48 +0000

    get /api/topics

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -4,8 +4,22 @@ const db = require("../db/connection.js");
 const seed = require("../db/seeds/seed");
 const { userData, topicData, commentData, articleData } = require("../db/data/test-data/index.js");
 
-describe("Dummy Test", ()=>{
-    test("Dummy Test", ()=>{
-        expect(1).toBe(1)
+beforeEach(() => seed({ topicData, userData, articleData, commentData }));
+afterAll(() => db.end());
+
+describe("GET /api/topics", ()=>{
+    test("200", ()=>{
+        return request(app)
+        .get("/api/topics")
+        .expect(200)
+        .then(({body: {topics}}) => {
+            expect(topics).toHaveLength(3)
+            topics.forEach(topic => {
+                expect(topic).toMatchObject({
+                    slug: expect.any(String),
+                    description: expect.any(String)
+                })
+            })
+        })
     })
 })
--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const { getTopics } = require("./cotrollers/topics.controller");
 const app = express();
 
 app.use(express.json())
+
+app.get("/api/topics", getTopics)
+
+module.exports = app;
