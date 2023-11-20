d7d41b310b99468a59f08ecc1d8d94e09c087496
Will <billyjoe2701@gmail.com>
2023-11-20 15:11:49 +0000

    Suggested Changed

---
+++ cotrollers/articles.controller.js
@@ -0,0 +1,21 @@
+const { selectArticleById, selectArticleComments } = require("../models/articles.model")
+
+exports.getArticleById = (req, res, next) => {
+    const {article_id} = req.params
+    selectArticleById(article_id).then(article => {
+        res.status(200).send({article})
+    }).catch(next)
+}
+
+exports.getArticles = (req, res, next) => {
+    selectArticles().then((articles)=>{
+        res.status(200).send({articles})
+    })
+}
+
+exports.getArticleComments = (req, res, next) => {
+    const {article_id} = req.params
+    selectArticleComments(article_id).then((comments)=>{
+        res.status(200).send({comments})
+    }).catch(next)
+}
---
+++ models/articles.model.js
@@ -0,0 +1,41 @@
+const db = require("../db/connection.js");
+
+exports.selectArticleById = (id) => {
+    return db.query(`SELECT * FROM articles WHERE article_id = $1`, [id]).then(({rows})=> {
+        if(!rows.length){
+            return Promise.reject({
+                status: 404,
+                msg: "Article not found"
+            })
+        }
+        return rows[0]
+    })
+}
+
+exports.selectArticles = () => {
+    return db
+      .query(
+        `SELECT articles.author,articles.title,articles.article_id,articles.topic,
+      articles.created_at,articles.votes,articles.article_img_url,CAST(COUNT(comments.comment_id)AS INT) AS comment_count
+  FROM articles
+  LEFT JOIN comments ON comments.article_id = articles.article_id
+  GROUP BY 
+      articles.article_id
+  ORDER BY articles.created_at DESC;`
+      )
+      .then(({ rows }) => {
+        return rows;
+      });
+  };
+
+  exports.selectArticleComments = (id) => {
+    return db.query(`SELECT * FROM comments WHERE article_id = $1`, [id]).then(({rows}) => {
+        if(!rows.length){
+            return Promise.reject({
+                status: 404,
+                msg: "Article not found"
+            })
+        }
+        return rows
+    })
+  }
