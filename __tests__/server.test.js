fc1d2a95cb4d1f123036c0a47c1ab26f9f65f456
Will <billyjoe2701@gmail.com>
2023-11-20 10:29:47 +0000

    Starts setting up the test environment

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,5 +1,11 @@
 const app = require('../server.js')
 const request = require("supertest");
-const db = require("../db/index.js");
-const seed = require("../db/seed");
+const db = require("../db/connection.js");
+const seed = require("../db/seeds/seed");
 const { userData, topicData, commentData, articleData } = require("../db/data/test-data/index.js");
+
+describe("Dummy Test", ()=>{
+    test("Dummy Test", ()=>{
+        expect(1).toBe(1)
+    })
+})
