4f2757a6358636701d47595a528db86a12e077d3
Will <billyjoe2701@gmail.com>
2023-11-22 08:26:13 +0000

    Suggested Changes

--- models/articles.model.js
+++ models/articles.model.js
@@ -33,32 +33,4 @@ exports.selectArticleComments = (id) => {
         }
     });
 
-//   return db
-//     .query(
-//       `SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC`,
-//       [id]
-//     )
-//     .then((response) => {
-//       console.log("Selecting all articles", response);
-//       if (!rows.length) {
-//         return db.query(`SELECT * FROM articles WHERE article_id = $1`, [id]);
-//       }
-//     })
-//     .then((response) => {
-//       console.log("Inside 2nd query");
-//       if (!response.length) {
-//         console.log("No Rows");
-//         return Promise.reject({
-//           status: 404,
-//           msg: "Article not found",
-//         });
-//       } else if (response.length === 0) {
-//         return [];
-//       } else {
-//         return response;
-//       }
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
 };
