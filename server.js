c2709993e9058e6cb90f920c703f5963cf6a781d
Will <billyjoe2701@gmail.com>
2023-12-11 11:36:41 +0000

    Allow CORS

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const express = require("express");
 const cors = require('cors')
 
-const { getTopics } = require("./cotrollers/topics.controller");
-const { getEndpoints } = require("./cotrollers/baseCall.controller");
-const { getArticleById, getArticleComments, getArticles, removeCommentById, getUsers , patchArticleById, postCommentByArticle } = require("./cotrollers/articles.controller");
-
 const { psqlErrors, customErrors } = require("./errors");
 const apiRouter = require("./routes/api-router");
 
 const app = express();
 
 app.use(express.json())
-app.use(cors)
+app.use(cors())
 
 app.use("/api", apiRouter)
 
